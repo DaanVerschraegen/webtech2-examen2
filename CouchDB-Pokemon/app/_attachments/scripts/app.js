0092ed4accd6e91ea21ce763e8bd67ce2938a328
DaanVerschraegen <daan.verschraegen@outlook.com>
2018-08-20 15:20:25 +0200

    op opstarten app documenten laden in pokemon2 (array van "type" moet nog worden aangemaakt

--- CouchDB-Pokemon/app/_attachments/scripts/app.js
+++ CouchDB-Pokemon/app/_attachments/scripts/app.js
@@ -11,23 +11,56 @@ angular.module('pokemonApp', ['ngRoute'])
 	        .otherwise({redirectTo:'/home'});
 	})
 		
-	.controller('pokemonCtrl', function($scope, pokemonSrv, saveSrv) {
+	.controller('pokemonCtrl', function($scope, pokemonSrv) {
+		pokemonSrv.getPokemonJson().then(function(data){
+			console.log(data);
+			var doc = {};
+			var pokemonObjects = data;
+			var pokemon = [];
+			for (var i = 0; i < pokemonObjects.length; i++) {
+				console.log(pokemonObjects[i]);
+				doc.name = pokemonObjects[i].name;
+				doc.type = pokemonObjects[i].type;
+				doc.trainer = pokemonObjects[i].trainer;
+				doc.gender = pokemonObjects[i].gender;
+				doc.owned = pokemonObjects[i].owned;
+				console.log(doc);
+				var json = JSON.stringify(doc);
+				console.log(json);
+				$.ajax({
+					type:			'PUT',
+					url:				'../../' + pokemonObjects[i].name,
+					data:			json,
+					contentType: 	'application/json',
+					dataType:		'json',
+					async:			true,
+					success:		function(data){
+						console.log('Load was performed.');
+					},
+					error:		function(XMLHttpRequest, textStatus, errorThrown){
+						console.log(errorThrown); 
+					}
+				});
+			}
+		}, function(err) { 
+			alert('Pokemon JSON file not found');
+		});
     })
    
     .service('pokemonSrv', function($http, $q) {
-    		this.getPokemon = function() {
+    		this.getPokemonJson = function() {
 	    		var q = $q.defer();
 	    		var url = 'http://localhost:5984/pokemon/c8160e335ea664dcc2ae92c83f001521/pokemon.json';
 	    		console.log(url);
 
 	    		$http.get(url)
 	    			.then(function(data){
-	    				//console.log(data);
-	    				var pokemonJson = data.data;
-	    				//console.log(pokemonObjects);
-	    				var pokemonObjects = [];
-	    				pokemonObjects.push(pokemonJson);
-	    				q.resolve(pokemonObjects);
+	    				console.log(data);
+	    				var pokemonJson = data.data.docs;
+	    				console.log(pokemonJson);
+	    				//var pokemonObjects = [];
+	    				//pokemonObjects.push(pokemonJson);
+	    				q.resolve(pokemonJson);
 	    				//
 	    			}, function(err) {
 	    				q.reject(err);
@@ -36,21 +69,3 @@ angular.module('pokemonApp', ['ngRoute'])
 	    			return q.promise;
 	    		};
     })
-    
-    .service('saveSrv', function($http, $q){
-		  this.setObject = function(key, value){
-			  $http.put('../../' + key, value);
-		  };
-		  
-		  this.getObject = function(key){
-			  var q = $q.defer();
-			  $http.get('../../' + key)
-	  			.then(function(data){
-	  				q.resolve(data.data);
-	  			}, function(err) {
-	  				q.reject(err);
-	  			});
-  			
-  			  return q.promise;
-		  };
-	});
